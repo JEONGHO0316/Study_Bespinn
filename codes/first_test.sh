29b8f0bcfcebf8110e33f2eeca795aaa9b8e8b4a
JEONGHO0316 <kjll290551@gmail.com>
2025-07-14 18:01:02 +0900

    첫번째 테스트

---
+++ codes/first_test.sh
@@ -0,0 +1,9 @@
+[jeongho@localhost ~]$ cd /home/jeongho/Music
+[jeongho@localhost Music]$ pwd
+/home/jeongho/Music
+[jeongho@localhost Music]$ ls
+[jeongho@localhost Music]$ cd ..
+[jeongho@localhost ~]$ cd /home/jeongho/Pictures
+[jeongho@localhost Pictures]$ pwd
+/home/jeongho/Pictures
+[jeongho@localhost Pictures]$ ls
