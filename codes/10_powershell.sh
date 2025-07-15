50ac155d899c1ba3a42ed5a8db0c59c64bfd02d2
JEONGHO0316 <kjll290551@gmail.com>
2025-07-15 12:23:13 +0900

    두번째_올리기

---
+++ codes/10_powershell.sh
@@ -0,0 +1,24 @@
+
+#C: 로컬 디스크로 위치변경 - > Develops 로 위치변경 후 ->현재 위치 확인
+PS C:\Users\Administrator> cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:58                Downloads
+d-----      2025-07-14   오후 6:11                VMs
+
+
+PS C:\Develops>
--- codes/first_test.sh
+++ codes/first_test.sh
@@ -1,8 +1,11 @@
+#Music으로 위치변경후 -> 현재위치 확인 및 파일 확인
 [jeongho@localhost ~]$ cd /home/jeongho/Music
 [jeongho@localhost Music]$ pwd
 /home/jeongho/Music
 [jeongho@localhost Music]$ ls
 [jeongho@localhost Music]$ cd ..
+
+#Pictures으로 위치변경후 -> 현재위치 확인 및 파일 확인
 [jeongho@localhost ~]$ cd /home/jeongho/Pictures
 [jeongho@localhost Pictures]$ pwd
 /home/jeongho/Pictures
